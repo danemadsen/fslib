db50158bfaa702a2612f1483b7839ca9dc1d9946
danemadsen <dane_madsen@hotmail.com>
2024-06-06 23:04:00 +1000

    cleanup

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ TST := test
 
 dir_guard=@mkdir -p $(@D)
 
-_OBJECTS := compile.o hash.o sr.o fst.o symt.o sort.o queue.o bitset.o print.o draw.o compose.o shortest.o match.o heap.o iter.o trim.o
+_OBJECTS := compile.o sr.o fst.o sort.o queue.o bitset.o print.o draw.o compose.o shortest.o match.o heap.o iter.o trim.o
 OBJECTS := $(patsubst %, $(OBJ)/%, $(_OBJECTS))
 
 TESTS := test_fst test_queue test_bitset test_print test_iter test_io test_compile test_sort test_hash test_stack test_trim test_compose test_heap test_shortest test_symt
--- src/bitset.h
+++ src/bitset.h
@@ -17,7 +17,7 @@ void                bitset_set(struct _bitset * bitset, int b);
 void                bitset_clear(struct _bitset * bitset, int b);
 int                 bitset_get(struct _bitset * bitset, int b);
 void                bitset_clear_all(struct _bitset * bitset);
-void                bitset_set_all();
+void                bitset_set_all(struct _bitset * bitset);
 void                bitset_union(struct _bitset * a, const struct _bitset * b);
 void                bitset_intersect(struct _bitset * a, const struct _bitset * b);
 struct _bitset *    bitset_toggle_all(struct _bitset * bitset);
